6d9b3c4eccc0ed1fc12204994d1c547679d6ba76
Rahul R <r.rahul.ug@gmail.com>
2020-11-17 14:03:46 +0530

    added sample shapes

---
+++ oab/Triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 17 13:52:47 2020
+
+@author: rahulr
+"""
+import Shape
+from math import sin,cos
+
+class Triangle(Shape):
+    
+    def populate_coords(self):
+        sides = self.sides
+        reso = (3.14/sides)
+        ptX = self.origin[0]
+        ptY = self.origin[1]
+        start_ang = self.angle
+        
+        coords = []
+        for i in range(sides):
+            ptX = ptX + sides * cos(start_ang + i * reso)
+            ptY = ptY + sides * sin(start_ang + i * reso)
+            coords.append((ptX, ptY))
+        
+        return coords
