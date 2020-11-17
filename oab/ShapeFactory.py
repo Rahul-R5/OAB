33cb2dee79796e3dcd8e3c6beb21874e3c989d21
Rahul R <r.rahul.ug@gmail.com>
2020-11-17 15:27:14 +0530

    finished minimum required functions

--- oab/ShapeFactory.py
+++ oab/ShapeFactory.py
@@ -6,14 +6,23 @@ Created on Mon Nov 16 00:25:09 2020
 @author: rahulr
 """
 import Map
+import random
+import Shape
+import Rectangle
+
+
 class ShapeFactory():
     """
     @param seed: seed for random function, int
     @param numObjects: number of objects initialised through factory, int    
     """
+    seed = 8
     numObjects = 0
     mapInstance = None  # Have a map class variable (i.e static variable)
     
+    """
+    Returns 'num' amount of shapes(obstacles) with random properties
+    """
     @staticmethod
     def getShapes(num):
         
@@ -21,29 +30,38 @@ class ShapeFactory():
         # TODO 
         # In future, avoid clipping obstacles with a mapInstance
         if ShapeFactory.mapInstance is None:
-            mapInstance = Map()
+            ShapeFactory.mapInstance = Map()
         
         # generate 'num' amount of objects and return them
         # increment the static object counter
         shapes = []
         for i in range(num):
-            shape = Shape()
+            origin = ShapeFactory.getRandomPos()
+            angle = ShapeFactory.getRandomAngle()
+            shape = Rectangle(origin, 4, angle)
             shapes.append(shape)
         
         ShapeFactory.numObjects += num
         return shapes    
         
-
-
-        
+            
     @staticmethod
     def getRandomPos():
-        raise NotImplementedError
+        posX = random.randint(0, ShapeFactory.mapInstance.grid[0])
+        posY = random.randint(0, ShapeFactory.mapInstance.grid[1])   
         
+        return (posX, posY)
+     
+    """  
+    Randomly selects the side of the shape needed from available shape
+    """
     @staticmethod
     def getRandomSide():
         raise NotImplementedError
         
     @staticmethod
-    def getRandomeAngle(num):
-        raise NotImplementedError
+    def getRandomAngle(num):
+        angle = random.randint(0, 359)
+        angle = (angle/180) * 3.14  # Convert to radians
+        
+        return angle
