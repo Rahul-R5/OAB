9f1bbc5cd174d9e17fb61590e026d572b9007f25
Rahul R <r.rahul.ug@gmail.com>
2020-11-18 23:19:20 +0530

    fixed angle resolution logic

--- oab/shape/Triangle.py
+++ oab/shape/Triangle.py
@@ -12,13 +12,13 @@ class Triangle(Shape):
     
     def populate_coords(self):
         sides = self.sides
-        reso = (3.14/sides)
+        reso = (2 * 3.14/3)
         ptX = self.origin[0]
         ptY = self.origin[1]
         start_ang = self.angle
         
         coords = []
-        for i in range(sides):
+        for i in range(3):
             ptX = ptX + sides * cos(start_ang + i * reso)
             ptY = ptY + sides * sin(start_ang + i * reso)
             coords.append((ptX, ptY))
