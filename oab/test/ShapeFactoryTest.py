e77cda9fee53b43f4aa76809cc6dbb38a5f7f89b
Rahul R <r.rahul.ug@gmail.com>
2020-11-18 23:26:56 +0530

    added draw method to plot closed shapes

--- oab/test/ShapeFactoryTest.py
+++ oab/test/ShapeFactoryTest.py
@@ -8,16 +8,38 @@ Created on Tue Nov 17 19:26:10 2020
 
 from oab.shape.ShapeFactory import ShapeFactory
 import matplotlib.pyplot as plt
+import matplotlib
+from matplotlib.patches import Polygon
+from matplotlib.collections import PatchCollection
+import numpy as np
 
 def run():
     num = 3
+    fig, ax = plt.subplots()
     shapes = ShapeFactory.getShapes(num) 
+    
     for shape in shapes:
         coords = shape.get_coords()
-        x = []
-        y = []
+        points = np.zeros((len(coords),2))
         for i in range(len(coords)):
-            x.append(coords[i][0])
-            y.append(coords[i][1])
-        plt.plot(x, y)
-   
+            points[i][0] = coords[i][0]
+            points[i][1] = coords[i][1]
+
+        draw(fig, ax, num, points)
+    plt.show()
+
+def draw(fig, ax, num, points):
+    
+    patches = []
+    num_polygons = 1
+    mapInstance = ShapeFactory.mapInstance
+    
+    polygon = Polygon(points, closed = True)
+    patches.append(polygon)
+
+    xmax = mapInstance.rows
+    ymax = mapInstance.columns
+    ax = plt.gca()
+    ax.add_patch(polygon)
+    ax.set_xlim(-10,xmax)
+    ax.set_ylim(-10,ymax)
