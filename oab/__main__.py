76eedbf14ddd870c795bd1e6a8f82b2529012952
Rahul R <r.rahul.ug@gmail.com>
2020-11-18 19:57:51 +0530

    fixed errors

--- oab/__main__.py
+++ oab/__main__.py
@@ -1,7 +1,9 @@
 # This the main file which will run the simulation.
 
 from oab.test.ShapeFactoryTest import run
+from oab.Map import Map
 
 if __name__ == "__main__":
+    maps = Map(0.5, (5, 5), 8)
     run()
     
