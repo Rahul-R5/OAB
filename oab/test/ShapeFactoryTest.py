5f45357e1c4eeb954aba6cff06f2630a0dcda972
Rahul R <r.rahul.ug@gmail.com>
2020-11-19 22:54:55 +0530

    changed to test class definition of test

--- oab/test/ShapeFactoryTest.py
+++ oab/test/ShapeFactoryTest.py
@@ -14,36 +14,37 @@ from matplotlib.collections import PatchCollection
 import numpy as np
 from oab.Map import Map
 from oab.MapInfo import MapInfo
-
-def run():
-    num = 3
-    mapInstance = Map(0.5, (50,50), 9)
-    MapInfo.setMap(mapInstance)
-    fig, ax = plt.subplots()
-    shapes = ShapeFactory.getShapes(num) 
+class ShapeFactoryTest():
+    def run(self):
+        num = 5
+        mapInstance = Map(0.5, (50,50), 9)
+        MapInfo.setMap(mapInstance)
+        fig, ax = plt.subplots()
+        shapes = ShapeFactory.getShapes(num) 
+        
+        for shape in shapes:
+            coords = shape.get_coords()
+            points = np.zeros((len(coords),2))
+            for i in range(len(coords)):
+                points[i][0] = coords[i][0]
+                points[i][1] = coords[i][1]
     
-    for shape in shapes:
-        coords = shape.get_coords()
-        points = np.zeros((len(coords),2))
-        for i in range(len(coords)):
-            points[i][0] = coords[i][0]
-            points[i][1] = coords[i][1]
-
-        draw(fig, ax, num, points)
-    plt.show()
-
-def draw(fig, ax, num, points):
+            self.draw(fig, ax, num, points)
+        plt.title("ShapeFactoryTest")
+        plt.show()
     
-    patches = []
-    num_polygons = 1
-    mapInstance = ShapeFactory.mapInstance
+    def draw(self, fig, ax, num, points):
+        
+        patches = []
+        num_polygons = 1
+        mapInstance = ShapeFactory.mapInstance
+        
+        polygon = Polygon(points, closed = True)
+        patches.append(polygon)
     
-    polygon = Polygon(points, closed = True)
-    patches.append(polygon)
-
-    xmax = mapInstance.rows
-    ymax = mapInstance.columns
-    ax = plt.gca()
-    ax.add_patch(polygon)
-    ax.set_xlim(-10,xmax)
-    ax.set_ylim(-10,ymax)
+        xmax = mapInstance.rows
+        ymax = mapInstance.columns
+        ax = plt.gca()
+        ax.add_patch(polygon)
+        ax.set_xlim(-10,xmax)
+        ax.set_ylim(-10,ymax)
