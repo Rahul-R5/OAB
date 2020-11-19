fd723e232e5e18750e83c70b3a0e88c7d90bec69
Rahul R <r.rahul.ug@gmail.com>
2020-11-19 19:12:15 +0530

    added SensorFactory class

---
+++ oab/SensorFactory.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 19 10:20:00 2020
+
+@author: rahulr
+"""
+import random
+from oab import Sensor
+from oab import MapInfo
+
+class SensorFactory():
+    
+    mapInstance = None
+    
+    @staticmethod
+    def getSensor():
+        if SensorFactory.mapInstance is None:
+            SensorFactory.mapInstance = MapInfo.mapInstance
+        
+        origin = SensorFactory.getRandomPos()
+        start_ang = SensorFactory.getRandomAngle()
+        ang_range = SensorFactory.getRadians(360)
+        
+        sensor = Sensor(origin, start_ang, ang_range, 10, 5)
+        return sensor
+        
+    
+    @staticmethod
+    def getRandomPos():
+        posX = random.randint(0, SensorFactory.mapInstance.rows)
+        posY = random.randint(0, SensorFactory.mapInstance.columns)   
+        
+        return (posX, posY)
+    
+    @staticmethod
+    def getRandomAngle():
+        angle = random.randint(0, 359)
+        angle = (angle/180) * 3.14  # Convert to radians
+        
+        return angle
+    
+    @staticmethod
+    def getRadians(degrees):
+        radians = (degrees/180) * 3.14  # Convert to radians
+        
+        return radians
