1b6b5f49686021ca86b146c2527f54bd8d5ce049
Rahul R <r.rahul.ug@gmail.com>
2020-11-26 22:52:05 +0530

    added test run command temporarily in setup

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
 # This is the file used to install the dependencies
+
+#import oab
+from oab.test.ShapeFactoryTest import ShapeFactoryTest
+from oab.test.SensorObstacleIntersectionTest import SensorObstacleIntersectionTest
+
+# test1 = ShapeFactoryTest()
+# test1.run()
+test2 = SensorObstacleIntersectionTest()
+test2.run()
