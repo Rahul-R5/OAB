b2239ec3b0cb532fbd063b609cc1ce4e5e56c39b
Rahul R <r.rahul.ug@gmail.com>
2021-02-07 20:19:20 +0530

    synced the random seed to mapInstance

--- oab/SensorFactory.py
+++ oab/SensorFactory.py
@@ -14,30 +14,42 @@ class SensorFactory():
     seed = 8
     mapInstance = None
     
+    # Try using **kwargs to get the sensor options
     @staticmethod
     def getSensor():
         if SensorFactory.mapInstance is None:
-            SensorFactory.mapInstance = MapInfo.mapInstance
+            SensorFactory.mapInstance = MapInfo.getMap()
             SensorFactory.seed = SensorFactory.mapInstance.seed
         
         origin = SensorFactory.getRandomPos()
         start_ang = SensorFactory.getRandomAngle()
         ang_range = SensorFactory.getRadians(90)
-        
-        sensor = Sensor(origin, start_ang, ang_range, 30, 10)
+        sensor_range = 20
+        num_rays = 10
+        sensor = Sensor(origin, start_ang, ang_range, sensor_range, num_rays)
         return sensor
         
-    
+    """
+    Returns a randomized initial coordinate (x,y) for a newly created sensor
+    Seed set by the map instance's seed for reproducibility
+    @return [posX,posY]: list of float
+    """
     @staticmethod
     def getRandomPos():
+        random.seed(SensorFactory.seed)
         posX = random.randint(0, SensorFactory.mapInstance.rows)
         posY = random.randint(0, SensorFactory.mapInstance.columns)   
         
-        #return [posX, posY]
-        return [20,20]
+        return [posX, posY]
     
+    """
+    Returns a randomized initial orientation (theta) between 0 and 360 for a newly created sensor
+    Seed set by the map instance's seed for reproducibility
+    @return angle: float(radian)
+    """
     @staticmethod
     def getRandomAngle():
+        random.seed(SensorFactory.seed)
         angle = random.randint(0, 359)
         angle = (angle/180) * 3.14  # Convert to radians
         
