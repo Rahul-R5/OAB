4b509b17ba57492adf3f84511cc770b52534e279
Rahul R <r.rahul.ug@gmail.com>
2021-02-07 20:18:52 +0530

    cleaned up getters and setters

--- oab/Sensor.py
+++ oab/Sensor.py
@@ -15,37 +15,41 @@ class Sensor():
         self.ang_range = ang_range
         self.length = length
         self.num_ray = num_ray
-        self.ray_pts = self.getEndPoints(self.origin, self.start_ang, 
-                                         self.ang_range, self.length,
-                                         self.num_ray)
+        self.ray_pts = []
+        self.set_pts()
+        
     """
     Calculates the end of the finite length sensor rays with given parameters
     and returns the 2D array
     @return pts: list of float list(2D array)
     """
-    def getEndPoints(self, origin, start_ang, ang_range, length, num_ray):
-        pts = []
-        reso = ang_range/num_ray
+    # NOTE: Can use **kwargs for keyword arguments passed. Better reaability
+    def set_pts(self):
+        pts = []        
+        length = self.length
+        start_ang = self.start_ang
+        num_ray = self.num_ray
+        reso = self.ang_range/num_ray
         for i in range(num_ray):
             
-            ptX = origin[0]
-            ptY = origin[1]
+            ptX = self.origin[0]
+            ptY = self.origin[1]
             ptX = ptX + length * cos(start_ang + i * reso)
             ptY = ptY + length * sin(start_ang + i * reso)
             pt = [ptX, ptY]
             
             pts.append(pt)
-            
-        return pts
+        
+        self.ray_pts = pts
+        
     
     """
     Moves the sensor to a different coordinate
     """
     def relocate(self, origin):
         self.origin = origin
-        self.ray_pts = self.getEndPoints(self.origin, self.start_ang, 
-                                         self.ang_range, self.length,
-                                         self.num_ray)
+        self.set_pts()
+
     """
     Rotates the sensor by given angle
     +ve angle: counter-clockwise rotation
@@ -53,9 +57,8 @@ class Sensor():
     """
     def rotate(self, angle):
         self.start_ang += angle
-        self.ray_pts = self.getEndPoints(self.origin, self.start_ang, 
-                                         self.ang_range, self.length,
-                                         self.num_ray)
+        self.set_pts()
+        
     """
     Returns the ray end points
     """    
