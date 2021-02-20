60b58916a8fa533a46fe41c324fbbc08c21e49ca
Rahul R <r.rahul.ug@gmail.com>
2021-02-20 19:38:39 +0530

    Modified robot class for path solving and animation
    Changes:-
    - added timestamps and states data member to track path of robot
    - tied the sensor to robot by combining method calls
    - added state setter
    - changed sensor management to use state setter
    - added movement actuators
    - added PathSolver class to calculate path to given destination
      * integrates derivatives to find control inputs
      * next state obtained from control inputs
    - added path solving to robot using PathSolver class
    - adjusted code to fit 100 character terminal width
    - added section decorators

--- oab/robot/Robot.py
+++ oab/robot/Robot.py
@@ -4,7 +4,13 @@ Created on Fri Jan  1 13:59:59 2021
 
 @author: Rahul R
 """
-from math import pi, cos, sin, atan2, sqrt
+import numpy as np
+from scipy.integrate import solve_ivp
+
+from numpy.linalg import inv
+from numpy import dot
+from math import pi, cos, sin, tanh, atan2, sqrt
+
 from oab.SensorFactory import SensorFactory
 
 # NOTE: Can use **kwargs for keyword arguments passed. Better reaability
@@ -16,6 +22,8 @@ class Robot():
         self.sensors = []
         self.coords = []         
         #self.actions =  self.populate_actions()
+        self.timestamps = np.array([])
+        self.states = np.array([])
         self.history = []
         self.populate_coords()
         
@@ -35,15 +43,28 @@ class Robot():
                 X = ptX + size * cos(start_ang + i * reso) 
                 Y = ptY + size * sin(start_ang + i * reso) 
             coords.append([X, Y])
-         
-        self.coords = coords
         
-            
+        # Update the associated sensor to the new robot position, if any    
+        if len(self.sensors) >= 1:
+            for sensor in self.sensors:
+                sensor.relocate(self.origin)
+                sensor.set_pts()  
+                
+        self.coords = coords
+     
+    def set_state(self,state):
+        self.origin = state[0:2]
+        self.angle = state[2]
+        self.populate_coords()
+        self.sensors[0].set_state(state)
+        
+    #--------------------------Sensor Management------------------------------        
     def add_sensor(self):
         sensor = SensorFactory.getSensor()
-        sensor.relocate(self.origin)
-        offset = self.angle - (sensor.start_ang + sensor.ang_range/2)
-        sensor.rotate(offset)
+        state = []
+        state.extend(self.origin)
+        state.append(self.angle)
+        sensor.set_state(state)
         self.sensors.append(sensor)
     
     def remove_sensor(self, index = None):
@@ -52,69 +73,59 @@ class Robot():
         else:
             del self.sensors[index]
         
-    ## Movement actuators
-    
-    def forward(self):
-        x,y = self.origin
-        self.origin = [x + cos(self.angle), y + sin(self.angle)]
-        self.history.append("F")
-        
-    def rightTurn(self):
-        self.angle -= pi/180
-        self.history.append("R")
-    
-    def backward(self):
-        x,y = self.origin
-        self.origin = [x - cos(self.angle), y - sin(self.angle)]
-        self.history.append("B")
-        
-    def leftTurn(self):
-        self.angle += pi/180
-        self.history.append("L")
-        
+    #-------------------------Movement actuators------------------------------  
     def rotate(self, angle):
         self.angle += angle
-        self.coords = self.populate_coords()
+        self.populate_coords()
         if len(self.sensors) > 0:
             for sensor in self.sensors:
                 sensor.rotate(angle)   
                 
-    ## Path algorithmns
-    
-    def moveto(self, dest):
-        origin = self.origin
-        dy = dest[1] - origin[1]
-        dx = dest[0] - origin[0]
-        angle = atan2(dy, dx)
-        ang_offset = angle - self.angle
-        self.rotate(ang_offset)
-        
-        
-        while(True):
-            if self._getL2Distance(self.origin, dest) < 1:
-                self.origin = dest
-                self.populate_coords()
+    def relocate(self, dest):
+        self.origin = dest
+        self.populate_coords()
+        if len(self.sensors) > 0:
+            for sensor in self.sensors:
+                sensor.relocate(dest)   
                 
-                # The sensor coords are not updated
-                self.sensors[0].relocate(self.origin)
-                self.sensors[0].set_pts()                
-                break
-            else:
-                self.forward()
-                self.populate_coords()
-                self.sensors[0].relocate(self.origin)
-                self.sensors[0].set_pts()
-    
+            
+    #--------------------------Path algorithmns-------------------------------
+    def moveto(self, dest):
         
+        # Given dest, set this dest in the controller of choice
+        solver = PathSolver()
+        
+        # Set the initial condition variables
+        x0 = self.origin[0]
+        y0 = self.origin[1]
+        theta0 = self.angle
+        start = np.array([x0, y0, theta0])
+        
+        # Solve the path with the initial conditions
+        [t,x] = solver.solve(start,dest)
+        
+        # Add the path to the history
+        if len(self.timestamps) == 0:
+            self.timestamps = t
+            self.states = x
+        else:
+            self.timestamps = np.append(self.timestamps,t)
+            self.states = np.append(self.states,x,axis = 0)
+            
+        # The path has been solved separately but the robot's 
+        # origin/orientation has not been updated for next move
+        self.origin = self.states[-1,0:2]
+        self.angle = self.states[-1,2]
+    
+    # Not used           
     def perform(self, index):
         # Take index and get action string from translate list
         action = getattr(str(index), lambda: "Invalid action")
         
+        # Take action string and execute function        
         action()
-        # Take action string and execute function
-        
     
-    ## Helper functions
+    #---------------------------Helper functions------------------------------
     
     def _getLineParam(self, A, B):
         M = (B[1] - A[1])/ (B[0] - A[0])
@@ -139,10 +150,107 @@ class Robot():
         return compressed
     
     def _getL2Distance(self, point1, point2):
-        return sqrt((point2[0] - point1[0]) * (point2[0] - point1[0]) + (point2[1] - point1[1]) * (point2[1] - point1[1]))
+        return sqrt((point2[0] - point1[0]) * (point2[0] - point1[0]) 
+                    + (point2[1] - point1[1]) * (point2[1] - point1[1]))
     
     def get_pts(self):
         return self.coords
-            
+    
+#------------------------Path Solving Algorithmns------------------------------            
+xd = 5
+yd = 5
+Dxd = 0
+Dyd = 0
+D2xd = -1
+D2yd = -1
+
+class PathSolver():
+    def _setDerivatives(self, **kwargs):
+        global xd
+        global yd
+        global Dxd
+        global Dyd
+        global D2xd
+        global D2yd
         
+        xd = kwargs["xd"]
+        yd = kwargs["yd"]
         
+    def solve(self, start, dest):
+        # Initial condition
+        init_state = start
+        self._setDerivatives(xd = dest[0], yd = dest[1])
+        # Simulation time
+        time = 100
+        
+        # Model limits        
+        # TODO:These limits need to be used
+        # v_limit = 2
+        # omega_limit = pi
+        
+        solver = "BDF"
+        
+        # Model solver
+        sol = solve_ivp(mobile_model, [0, time], init_state, method=solver, dense_output=True)
+        t = np.array(sol.t)
+        x = np.transpose(sol.y)
+        return [t,x]
+
+def mobile_model(t,x):
+    # States extracted
+    theta = x[2]
+    
+    # Get control inputs
+    [v, omega] = CL_controller(t, x)
+
+    # Model evaluated
+    next_state = [ v*cos(theta),
+                   v*sin(theta),
+                   omega ]
+    
+    return next_state
+        
+def CL_controller(t, x):
+    # Variables
+    pt_offset = 0.2;
+    K1 = 5;
+    K2 = K1;
+    
+    # Not Used
+    # a = 2;
+    # c = 0.05;
+    # b = 3 * c;
+    
+    x_pos = x[0]
+    y_pos = x[1]
+    theta = x[2]
+     
+    # Trajectory calculated 
+    # Taken from global variables
+    
+    P = [x_pos + pt_offset * cos(theta), y_pos + pt_offset * sin(theta)]
+    #The look ahead point is tracking the trifolium trajectory
+    Pd = [xd, yd]          
+    DPd = [Dxd, Dyd] 
+    
+    A = [[cos(theta), (-1*pt_offset * sin(theta))], 
+         [sin(theta), (pt_offset * cos(theta))]]
+    
+    # e_t = P - Pd 
+    e_0 = P[0] - Pd[0]   
+    e_1 = P[1] - Pd[1]
+    e_t = []
+    e_t.append(tanh(e_0))
+    e_t.append(tanh(e_1))
+    
+    #K1 and K2 can be different since they are different inputs
+    u1 = -1 * K1 * e_t[0] 
+    u2 = -1 * K2 * e_t[1]
+    
+    # Control inputs calculated 
+    control = dot(inv(A), [DPd[0] + u1, DPd[1] + u2]) 
+    
+    v = control[0]
+    omega = control[1]
+    
+    return [v, omega]
